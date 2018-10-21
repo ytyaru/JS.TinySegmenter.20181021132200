6dc4c2948694f926c34e923cc55f3df147588769
ytyaru <ytyaru@outlook.jp>
2018-10-21 14:41:08 +0900

    リアルタイム変換＆フォーカス設定

--- docs/setup.js
+++ docs/setup.js
@@ -1,7 +1,11 @@
 window.addEventListener('DOMContentLoaded', function() {
 	var jpstr = document.getElementById("JapaneseString");
 	jpstr.addEventListener("change", Run);
+	jpstr.addEventListener("keyup", Run);
+	jpstr.addEventListener("paste", Run);
+	jpstr.addEventListener("input", Run);
 	jpstr.value = "私の名前は中野です。";
+	document.forms.UserForm.JapaneseString.focus();
 	Run();
 });
 function Run() {
